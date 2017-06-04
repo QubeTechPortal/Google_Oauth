6a79070743d4a90898fcf55b1547a09e92dd68dd
harish120896 <saiharish120896@gmail.com>
2017-06-04 17:18:11 +0530

    initial

---
+++ app/models/user.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+
+var userSchema = mongoose.Schema({
+	local: {
+		username: String,
+		password: String
+	},
+	facebook: {
+		id: String,
+		token: String,
+		email: String,
+		name: String
+	},
+	google: {
+		id: String,
+		token: String,
+		email: String,
+		name: String
+	}
+});
+
+userSchema.methods.generateHash = function(password){
+	return password;
+}
+
+userSchema.methods.validPassword = function(password){
+	return true;
+}
+
+module.exports = mongoose.model('User', userSchema);
---
+++ config/auth.js
@@ -0,0 +1,13 @@
+module.exports = {
+	'facebookAuth' : {
+		'clientID': 'enter client id here',
+		'clientSecret': 'enter client secret here',
+		'callbackURL': 'http://localhost:8080/auth/google/callback'
+	},
+
+	'googleAuth' : {
+		'clientID': 'enter client id',
+		'clientSecret': 'enter client secret',
+		'callbackURL': 'http://localhost:8080/auth/google/callback'
+	}
+}
